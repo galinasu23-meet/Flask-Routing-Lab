f40f957e796c3ce7f5ef1fda224a0bb69adaab16
galinasu23-meet <109904723+galinasu23-meet@users.noreply.github.com>
2022-07-26 12:47:23 +0300

    i need to do the bonuss

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -1,18 +1,18 @@
-from flask import Flask, redirect, request, render_template, url_for
 
 
-app = Flask(  # Create a flask app
-    __name__,
-    template_folder='templates',  # Name of html file folder
-    static_folder='static'  # Name of directory for static files
-)
+from flask import Flask , render_template
+app = Flask(__name__)
 
-# Your code should be below
+@app.route('/')
+def main():
+    return render_template('home.html')
+ 
+@app.route('/product')
+def product():
+    return render_template('product.html')
 
-
-
-
-# Your code should be above
-
-if __name__ == "__main__":  # Makes sure this is the main process
-    app.run(debug=True)
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+ 
+app.run(debug = True)
